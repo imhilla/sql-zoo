a5bb0b93a1306d1c9ae2950114807845a7c4e5a0
Hillary Kiptoo <hillaryodhiambo282@gmail.com>
2020-05-28 12:54:40 +0300

    Update SELECT-from-nobel.sql

--- SELECT-from-nobel.sql
+++ SELECT-from-nobel.sql
@@ -13,3 +13,26 @@ SELECT yr, subject, winner
 SELECT yr, subject
 FROM nobel
 WHERE winner = 'Albert Einstein';
+
+--Give the name of the 'Peace' winners since the year 2000, including 2000.
+SELECT winner
+FROM nobel
+WHERE subject = 'Peace' AND yr >= 2000
+
+--Show all details (yr, subject, winner) of the Literature prize winners for 1980 to 1989 inclusive
+SELECT yr, subject, winner
+FROM nobel
+WHERE subject= 'Literature' AND yr BETWEEN 1980 AND 1989
+
+--Show all details of the presidential winners:
+--Theodore Roosevelt
+--Woodrow Wilson
+--Jimmy Carter
+--Barack Obama
+SELECT * FROM nobel
+ WHERE winner IN ('Theodore Roosevelt', 'Woodrow Wilson', 'Jimmy Carter', 'Barack Obama') 
+
+--Show the winners with first name John
+SELECT winner
+FROM nobel
+WHERE winner LIKE 'John%'
