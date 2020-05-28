cc2e18a7ae913fbe76ed49b0b618491acb3a35ff
Hillary Kiptoo <hillaryodhiambo282@gmail.com>
2020-05-28 16:11:54 +0300

    Update SELECT-within-SELECT.sql

--- SELECT-within-SELECT.sql
+++ SELECT-within-SELECT.sql
@@ -16,3 +16,21 @@ WHERE name='united kingdom')
 
 --List the name and continent of countries in the 
 --continents containing either Argentina or Australia. Order by name of the country.
+SELECT name, continent 
+FROM world
+WHERE continent IN(SELECT continent from world WHERE name IN ('Argentina', 'Australia'))
+Order by name
+
+--Which country has a population that is more than Canada but less than Poland? 
+--Show the name and the population.
+SELECT name, population FROM world
+WHERE population>(SELECT population FROM world WHERE name = 'Canada' ) 
+AND population <(SELECT population FROM world WHERE name = 'Poland')
+
+--Show the name and the population of each country in Europe. 
+--Show the population as a percentage of the population of Germany.
+SELECT name, CONCAT(ROUND(100*population/ (SELECT population 
+                                              FROM world 
+                                                WHERE name = 'Germany'), 0),'%')
+FROM world
+WHERE continent = 'Europe'
