99699c94cf62e9ba87a841246c27fce00bcebe0d
Hillary Kiptoo <hillaryodhiambo282@gmail.com>
2020-05-28 13:48:56 +0300

    Update SELECT-from-nobel.sql

--- SELECT-from-nobel.sql
+++ SELECT-from-nobel.sql
@@ -36,3 +36,42 @@ SELECT * FROM nobel
 SELECT winner
 FROM nobel
 WHERE winner LIKE 'John%'
+
+--Show the year, subject, and name of Physics winners for 1980 together with the Chemistry winners for 1984.
+SELECT yr, subject, winner
+FROM nobel
+WHERE subject = 'Physics' AND yr = 1980 OR subject = 'Chemistry' AND yr = 1984
+--Show the year, subject, and name of winners for 1980 excluding Chemistry and Medicine
+
+--Show the year, subject, and name of winners for 1980 excluding Chemistry and Medicine
+SELECT yr, subject, winner
+FROM nobel
+WHERE yr = 1980 and subject NOT IN ('Chemistry', 'Medicine')
+
+--Show year, subject, and name of people who won a 'Medicine' prize in an early 
+--year (before 1910, not including 1910)
+-- together with winners of a 'Literature' prize in a later year (after 2004, including 2004)
+
+--Show year, subject, and name of people who won a 'Medicine' prize in an early year 
+--(before 1910, not including 1910) 
+--together with winners of a 'Literature' prize in a later year (after 2004, including 2004)
+SELECT yr, subject, winner
+FROM nobel
+WHERE subject = 'Medicine' AND  yr < 1910 OR subject = 'Literature' AND yr>=2004
+
+--Find all details of the prize won by PETER GRÜNBERG
+SELECT * 
+FROM nobel
+WHERE winner = 'PETER GRÜNBERG'
+
+--Find all details of the prize won by EUGENE O'NEILL
+SELECT *
+FROM nobel
+WHERE winner = "EUGENE O\'NEILL"
+
+--List the winners, year and subject where the winner starts with Sir. 
+--Show the the most recent first, then by name order.
+SELECT winner, yr, subject
+FROM nobel
+WHERE winner LIKE 'sir%'
+ORDER BY yr DESC, winner
