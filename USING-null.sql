47301c996bb7bb3ada00aa07fedc63eb8c6c0f8c
Hillary Kiptoo <hillaryodhiambo282@gmail.com>
2020-05-29 14:34:21 +0300

    Update USING-null.sql

--- USING-null.sql
+++ USING-null.sql
@@ -2,3 +2,19 @@
 SELECT name
 FROM teacher
 WHERE dept IS NULL
+
+--Note the INNER JOIN misses the teachers with no department and the departments with no teacher.
+SELECT teacher.name, dept.name
+ FROM teacher INNER JOIN dept
+           ON (teacher.dept=dept.id)
+
+--Use a different JOIN so that all teachers are listed.
+SELECT teacher.name 'Teacher', dept.name 'Department'
+FROM teacher LEFT JOIN dept
+        ON (teacher.dept=dept.id)
+
+--Use a different JOIN so that all departments are listed.
+
+SELECT teacher.name 'Teacher', dept.name 'Department'
+FROM teacher RIGHT JOIN dept
+        ON (teacher.dept=dept.id)
